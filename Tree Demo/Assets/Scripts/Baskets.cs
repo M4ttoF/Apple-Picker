eca73b686f834eca6ed626146e8220736004b908
Matthew Farias <ezioblade64@gmail.com>
2018-01-21 08:47:04 -0500

    Init Commit

---
+++ Tree Demo/Assets/Scripts/Baskets.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Baskets : MonoBehaviour {
+
+	private Vector3 moveTo;
+
+	private int chiCnt;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveTo = new Vector3 (Mathf.Clamp(Input.mousePosition.x ,116,890),189f,0f);
+		print (Input.mousePosition.x);
+		transform.position = moveTo;
+	}
+
+	void OnTriggerEnter2D(){
+		Score.scoreValue += 1;
+		SoundScript.PlaySound ("Hit");
+	}
+
+	public void Fail(){
+		chiCnt = transform.childCount;
+		if (chiCnt == 0) {
+			Destroy (gameObject);
+		} else {
+			Destroy(transform.GetChild (chiCnt - 1));
+		}
+	}
+}
---
+++ Tree Demo/Assets/Scripts/SoundScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour {
+
+	public static AudioClip hitSound, missSound, failSound;
+	static AudioSource audioSrc;
+	// Use this for initialization
+	void Start () {
+		hitSound = Resources.Load<AudioClip> ("Hit");
+		missSound = Resources.Load<AudioClip> ("Miss");
+		failSound = Resources.Load<AudioClip> ("Fail");
+	
+		audioSrc=GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public static void PlaySound(string clip){
+		switch (clip) {
+		case "Hit":
+			audioSrc.PlayOneShot (hitSound);
+			break;
+		case "Miss":
+			audioSrc.PlayOneShot (missSound);
+			break;
+		case "Fail":
+			audioSrc.PlayOneShot (failSound);
+			break;
+		}
+	}
+}
